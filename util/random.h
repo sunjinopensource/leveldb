beb59ca7e0eaed52d5b478b6b2cc4578e05bf3ef
jinsun <jinsun@example.com>
2023-08-29 16:22:16 +0800

    OneIn

--- util/random.h
+++ util/random.h
@@ -50,7 +50,7 @@ class Random {
 
   // Randomly returns true ~"1/n" of the time, and false otherwise.
   // REQUIRES: n > 0
-  bool OneIn(int n) { return (Next() % n) == 0; }
+  bool OneIn(int n) { return Uniform(n) == 0; }
 
   // Skewed: pick "base" uniformly from range [0,max_log] and then
   // return "base" random bits.  The effect is to pick a number in the
